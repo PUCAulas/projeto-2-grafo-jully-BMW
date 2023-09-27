432336729fa9b58f96c304ba59e619a577438970
Jully Silva <jullyksilva@hotmail.com>
2023-09-27 18:34:58 -0300

    [Grafo]: Ajuste no metodo de criar o grafo

--- codigo/grafo/src/main/java/com/grafos/grafo/Grafo.java
+++ codigo/grafo/src/main/java/com/grafos/grafo/Grafo.java
@@ -8,18 +8,17 @@ import java.util.Map;
 import java.util.Set;
 
 import com.grafos.aresta.Aresta;
+import com.grafos.vertice.Vertice;
 
 public class Grafo {
-    private Map<String, List<Aresta>> listaDeCidades = new HashMap<>();
-    public List<Aresta> arestas = new ArrayList<>();
+    private Map<String, ArrayList<Aresta>> listaDeCidades = new HashMap<String, ArrayList<Aresta>>();
+    static ArrayList<Aresta> arestas = new ArrayList<>();
 
-    public void adicionarCidade(String cidade, List<String> conexoes) {
-        List<Aresta> arestas = new ArrayList<>();
+    public void adicionarCidade(String cidade, String[] conexoes) {
         for (String conexao : conexoes) {
-            String[] partes = conexao.split("\\s*\\(\\s*|\\s*\\)\\s*,\\s*");
-            String cidadeDestino = partes[0];
-            int peso = Integer.parseInt(partes[1]);
-            arestas.add(new Aresta(cidadeDestino, peso));
+            String[] dadosConexao = conexao.trim().split("\\(");
+            int distancia = Integer.parseInt(dadosConexao[1].replaceAll("[^0-9]", ""));
+            arestas.add(new Aresta((new Vertice(cidade)), (new Vertice(dadosConexao[0])), distancia));
         }
         listaDeCidades.put(cidade, arestas);
     }
@@ -36,28 +35,28 @@ public class Grafo {
         return false;
     }
 
-    public List<String> cidadesInalcancaveis(String cidadeOrigem) {
-        List<String> cidadesInalcancaveis = new ArrayList<>();
-        Set<String> visitadas = new HashSet<>();
-        visitarCidades(cidadeOrigem, visitadas);
-        for (String cidade : listaDeCidades.keySet()) {
-            if (!visitadas.contains(cidade)) {
-                cidadesInalcancaveis.add(cidade);
-            }
-        }
-        return cidadesInalcancaveis;
-    }
+    // public List<String> cidadesInalcancaveis(String cidadeOrigem) {
+    //     List<String> cidadesInalcancaveis = new ArrayList<>();
+    //     Set<String> visitadas = new HashSet<>();
+    //     visitarCidades(cidadeOrigem, visitadas);
+    //     for (String cidade : listaDeCidades.keySet()) {
+    //         if (!visitadas.contains(cidade)) {
+    //             cidadesInalcancaveis.add(cidade);
+    //         }
+    //     }
+    //     return cidadesInalcancaveis;
+    // }
 
-    private void visitarCidades(String cidade, Set<String> visitadas) {
-        visitadas.add(cidade);
-        if (listaDeCidades.containsKey(cidade)) {
-            for (Aresta aresta : listaDeCidades.get(cidade)) {
-                if (!visitadas.contains(aresta.getDestino())) {
-                    visitarCidades(aresta.getDestino(), visitadas);
-                }
-            }
-        }
-    }
+    // private void visitarCidades(String cidade, Set<String> visitadas) {
+    //     visitadas.add(cidade);
+    //     if (listaDeCidades.containsKey(cidade)) {
+    //         for (Aresta aresta : listaDeCidades.get(cidade)) {
+    //             if (!visitadas.contains(aresta.getDestino())) {
+    //                 visitarCidades(aresta.getDestino(), visitadas);
+    //             }
+    //         }
+    //     }
+    // }
 
     public String toString(String cidade) {
         if (!listaDeCidades.containsKey(cidade)) {
@@ -68,12 +67,11 @@ public class Grafo {
         texto.append("Cidade: ").append(cidade).append("\n");
         texto.append("Conexões: \n");
 
-        List<Aresta> conexoes = listaDeCidades.get(cidade);
+        ArrayList<Aresta> conexoes = listaDeCidades.get(cidade);
         for (Aresta aresta : conexoes) {
-            texto.append(" ---> ").append(aresta.getDestino()).append(" (Peso: ").append(aresta.getPeso())
+            texto.append(" ---> ").append(aresta.getDestino().getNome()).append(" (Peso: ").append(aresta.getDistancia())
                     .append(")\n");
         }
-
         return texto.toString();
     }
 
