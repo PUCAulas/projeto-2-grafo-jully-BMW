4230010efb6a13f4d9d41ad7aa6b40ce1d123792
Jully Silva <jullyksilva@hotmail.com>
2023-09-27 18:33:53 -0300

    [Vertice]: Criacao da classe Vertice para aplicacao

---
+++ codigo/grafo/src/main/java/com/grafos/vertice/Vertice.java
@@ -0,0 +1,17 @@
+package com.grafos.vertice;
+
+public class Vertice {
+    private String nome;
+    
+    public Vertice(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
